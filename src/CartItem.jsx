33b90a34cdf131220875068e43adf14b9c4300f1
epperic <epperic@yahoo.de>
2025-03-11 07:38:36 -0400

    bugfixes

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,10 +9,11 @@ const CartItem = ({ onContinueShopping }) => {
 
     // Calculate total amount for all products in the cart
     const calculateTotalAmount = () => {
-        let total;
+        let total = 0;
         cart.forEach((item) => {
             const { cost, quantity } = item;
-            total = total + (quantity * parseFloat(cost.substring(1)))
+            const realCost = parseFloat(cost.substring(1));
+            total = total + (quantity * realCost)
         })
         return total
     };
@@ -29,11 +30,11 @@ const CartItem = ({ onContinueShopping }) => {
         if (item.quantity > 1)
             dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
         else
-            dispatch(removeItem());
+            dispatch(removeItem(item.name));
     };
 
     const handleRemove = (item) => {
-        dispatch(handleRemove(item));
+        dispatch(removeItem(item.name));
     };
 
     // Calculate total cost based on quantity for an item
